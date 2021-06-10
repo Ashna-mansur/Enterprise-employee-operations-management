3d106aa218120f00e2c2edcef31038390e200189
Ashna-mansur <71560139+Ashna-mansur@users.noreply.github.com>
2021-06-10 23:28:20 +0530

    Add files via upload

---
+++ zdivsn.py
@@ -0,0 +1,14 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+if (a>b):
+    if(b==0):
+        print("not defined")
+     else:
+        div=a/b
+        print(div)
+else:
+    if(a==0):
+        print("not defined")
+            else:
+                div=b/a
+                print(div)
