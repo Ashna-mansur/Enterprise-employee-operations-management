dfa68ac862d90666f5e09bd2575e2242aa92af23
Ashna-mansur <71560139+Ashna-mansur@users.noreply.github.com>
2021-06-16 12:42:26 +0530

    Add files via upload

---
+++ today.py
@@ -0,0 +1,16 @@
+l=[1,2,3]
+t=(1,2,3)
+d={"name":"ashna","age":23}
+c={5,7,"ash"}
+print(d)
+print(d["name"])
+d["name"]="anu"
+print(d["name"])
+
+
+s={}
+a=input("enter name")
+b=int(input("age"))
+s["name"]=a
+s["age"]=b
+print(s)
